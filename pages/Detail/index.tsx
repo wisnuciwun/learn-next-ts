a77dc4021a011709522d9e424898c1d162af58eb
wisnuciwun <adiwardhanawisnu@gmail.com>
2021-11-18 21:45:42 +0700

    learn part 9 - dynamic route and getstaticprops

--- pages/Detail/index.tsx
+++ pages/Detail/index.tsx
@@ -1,9 +1,34 @@
-export default function Detail() {
+// interface DataProps{
+//     data: [{name: '', email: ''}];
+// }
+
+export default function Detail({ data = [{name: '', email: ''}] }) {
+    console.log(data)
     return (
         <div className="container">
             <div className="element fade-in">
-                Coding is never be easy, but enjoy it because you'll ends happy
+                <b>Coding is never be easy, but enjoy it because you'll ends happy</b>
+                <br /><br />
+                Jsonplaceholder trials be like this :
+                {
+                    data.map(x => {
+                        return(
+                            <p>{x.name} - {x.email}</p>            
+                        )
+                    })
+                }
             </div>
         </div>
     )
+}
+
+export async function getStaticProps() {
+    //untuk data yg statis
+    const result = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await result.json()
+    return {
+        props: {
+            data
+        }
+    }
 }
